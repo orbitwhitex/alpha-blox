e73f9d5bef81d8530a3386b99d2530663adf2bc4
Udemy Course <orbitwhitex@gmail.com>
2017-06-03 19:01:56 +0000

    Complete authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -23,7 +25,7 @@ class ArticlesController < ApplicationController
     
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       #do something
       flash[:success] = "Article was successfully created"
@@ -46,10 +48,20 @@ class ArticlesController < ApplicationController
   
   
   private
+    
     def set_article
       @article = Article.find(params[:id])
     end
+    
     def article_params
       params.require(:article).permit(:title, :description)
     end
+    
+    def  require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to article_path(@article)
+      end
+    end
+
 end
