791cd5ed1b0343ace3debbd87731893903a0b803
Udemy Course <orbitwhitex@gmail.com>
2017-06-03 20:35:17 +0000

    Added Admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -13,7 +14,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Alpha Blog, #{@user.username}!"
-      redirect_to articles_path
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
       render 'new'
     end
@@ -25,16 +27,22 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
-      redirect_to articles_path
+      redirect_to root_path
     else
       render 'edit'
     end
   end
   
   def show
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
   
   private
   
@@ -47,12 +55,16 @@ class UsersController < ApplicationController
   end
   
   def require_same_user
-    if !logged_in?
-      flash[:danger] = "Access Restricted"
-      redirect_to root_path
-    else current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
   end
+  
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can delete a user"
+    end
+  end
+  
 end
